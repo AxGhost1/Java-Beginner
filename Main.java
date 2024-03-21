e733a57f0b2421b7210155d4e414013d42e180f2
AxGhost1 <155352166+AxGhost1@users.noreply.github.com>
2024-03-21 17:39:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // Printing In Console
+		
+		int x; // Declaration Variable
+		x = 123; // Assign Variable
+		
+		int y = 234; // Initialize Variable
+		System.out.println(x);
+		System.out.println(y);
+		
+		// Swap Variables
+		
+		String a = "Hello";
+		String b = "World";
+		String temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		
+		// Taking User Input
+		
+		
+		
+	}
+
+}
